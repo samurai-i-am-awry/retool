786a16215af969032a8d0cd0e4d490aeb8259213
ahalloran <ahalloran01@gmail.com>
2019-01-23 19:18:15 -0600

    final touches

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
 const toolRoutes = require("./tool");
-const toolsController = require("../../controllers/toolsController");
-
 
 // Book routes
 router.use("/tool", toolRoutes);
