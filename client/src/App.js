188cf45f7d49695bb0426183bb91bc997d37398a
ahalloran <ahalloran01@gmail.com>
2019-01-23 19:26:59 -0600

    final final push

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,9 @@ import NoMatch from "./pages/NoMatch";
 import Profile from "./pages/Profile";
 import Search from "./pages/Search";
 import ToolEntry from "./pages/ToolEntry";
-<<<<<<< HEAD
 import TestTool from "./pages/TestTool";
-=======
 import Toolbox from "./pages/Toolbox";
 import About from "./pages/About";
->>>>>>> 797c92687a8a4cb92ad2a1322b6ae95f561bd0a1
 
 
 function App() {
@@ -30,13 +27,10 @@ function App() {
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/toolentry" component={ToolEntry} />
           <Route exact path="/search" component={Search} />
-<<<<<<< HEAD
           <Route exact path="/test" component={TestTool
           } />
-=======
           <Route exact path="/toolbox" component={Toolbox} />
           <Route exact path="/about" component={About} />
->>>>>>> 797c92687a8a4cb92ad2a1322b6ae95f561bd0a1
           <Route component={NoMatch} />
         </Switch>
       </div>
